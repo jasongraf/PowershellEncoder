7840cf81a1bceeca7f0810b9c960b34ab52d17d9
jasongraf <jason@grafville.com>
2017-06-29 15:46:09 -0400

    Add files via upload

---
+++ Powershell Encoder.ps1
@@ -0,0 +1,4 @@
+﻿$t=Read-Host -Prompt 'Input Powershell Command to Encode'
+$Bytes = [System.Text.Encoding]::Unicode.GetBytes($t)
+$EncodedText =[Convert]::ToBase64String($Bytes)
+Write-Host "powershell.exe -nop -w hidden -enc $EncodedText"
